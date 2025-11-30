f0bb5ed1c7802b2dd8333707158d3ac814d98fe6
AKRISHNASRIKAR <akrishnasrikar@gmail.com>
2025-12-01 01:53:56 +0530

    Page and dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import HeroSection from "@/components/home/Hero";
 import ProductSection from "@/components/home/product-section";
-import Testimonials from "@/components/home/testemonials";
-import BestSellersSection from "@/components/home/best-sellers-section";
-import TrustSection from "@/components/home/trust-section";
-import CompanyMessageSection from "@/components/home/company-message-section";
-import FeaturesSection from "@/components/home/features-section";
+import Testimonials from "@/components/home/testimonials";
+import BestSellersSection from "@/components/home/best-sellers";
+import TrustSection from "@/components/home/trust";
+import FeaturesSection from "@/components/home/features";
 import CharitableInitiativesSection from "@/components/home/charitables";
 
 export default function Home() {
@@ -14,7 +13,6 @@ export default function Home() {
       <ProductSection />
       <BestSellersSection />
       <TrustSection />
-      <CompanyMessageSection />
       <FeaturesSection />
       <CharitableInitiativesSection />
       <Testimonials />
